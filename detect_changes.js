5b5abc4dd9b179efac45bd6fd74199946a5b52b6
Saheb Roy <theioscracker@gmail.com>
2023-12-26 23:12:05 +0100

    check

--- detect_changes.js
+++ detect_changes.js
@@ -1,13 +1,19 @@
 const { execSync } = require('child_process');
 
 // Get the list of changed files in the last commit
-const changedFiles = execSync('git diff --name-only HEAD^ HEAD')
+const changedFiles = execSync('git diff --name-only HEAD~1 --')
   .toString()
   .split('\n')
   .filter(Boolean);
 
+// Debugging output
+console.log('Changed Files:', changedFiles);
+
 // Filter out only the .swift files
 const swiftFiles = changedFiles.filter(file => file.endsWith('.swift'));
+
+// Debugging output
+console.log('Swift Files:', swiftFiles);
 
 // Create a report of changed .swift files
 const report = `Changed Swift Files:\n${swiftFiles.join('\n')}`;
