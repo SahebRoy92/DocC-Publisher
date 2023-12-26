f6e03d6d27f26aec9245035de6cb5a5f16be37c1
Saheb Roy <theioscracker@gmail.com>
2023-12-26 21:55:15 +0100

    updated

--- DocCPublisherDemo/DocCPublisherDemo/DemoTextfield.swift
+++ DocCPublisherDemo/DocCPublisherDemo/DemoTextfield.swift
@@ -94,6 +94,14 @@ public class KTextField: UITextField {
         }
     }
     
+    /// Applies border to the text view with the specified width
+    @IBInspectable public var borderWidthNEWBORDER12: CGFloat = 0.0 {
+        didSet {
+            layer.borderWidth = borderWidthNEWBORDER12
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
     /// Applies a new borderWidth to the text view with the specified width
     @IBInspectable public var newBorderWidth: CGFloat = 0.0 {
         didSet {
