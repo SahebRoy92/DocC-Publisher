591a1f532c6816d0994166c55f802316c7c6c445
Saheb Roy <theioscracker@gmail.com>
2023-12-26 21:44:58 +0100

    changed py file

--- DocCPublisherDemo/DocCPublisherDemo/DemoTextfield.swift
+++ DocCPublisherDemo/DocCPublisherDemo/DemoTextfield.swift
@@ -78,6 +78,14 @@ public class KTextField: UITextField {
         }
     }
     
+    /// Applies border to the text view with the specified width
+    @IBInspectable public var borderWidthNEWBORDER10: CGFloat = 0.0 {
+        didSet {
+            layer.borderWidth = borderWidthNEWBORDER10
+            layer.borderColor = borderColor.cgColor
+        }
+    }
+    
     /// Applies a new borderWidth to the text view with the specified width
     @IBInspectable public var newBorderWidth: CGFloat = 0.0 {
         didSet {
--- detect_changes.py
+++ detect_changes.py
@@ -1,26 +1,20 @@
 import os
 from git import Repo
 
-# Path to the local git repository
-repo_path = "/path/to/your/repo"
-
-# Path to the main branch
-branch_name = "main"
-
 # Open the repository
-repo = Repo(repo_path)
+repo = Repo(os.getcwd())  # Using os.getcwd() to get the current working directory
 
 # Fetch changes from the remote repository
 repo.remotes.origin.fetch()
 
 # Get the main branch
-main_branch = repo.branches[branch_name]
+main_branch = repo.branches['main']  # Using the branch name directly
 
 # Get the commit at the tip of the main branch
 head_commit = main_branch.commit
 
 # Get the commit at the tip of the main branch on the remote repository
-remote_commit = repo.remotes.origin.refs[branch_name].commit
+remote_commit = repo.remotes.origin.refs['main'].commit  # Using the branch name directly
 
 # Get the changes between the local and remote main branches
 changed_files = repo.git.diff(remote_commit, head_commit, name_only=True).splitlines()
