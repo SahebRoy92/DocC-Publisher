87eccda3a63119c691bbad94c9b4d007b459c9a1
Saheb Roy <theioscracker@gmail.com>
2023-12-26 23:52:28 +0100

    Updated!

--- DocCPublisherDemo/DocCPublisherDemo/ViewController.swift
+++ DocCPublisherDemo/DocCPublisherDemo/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        print("Added viewWillAppear ---15-")
+        print("Added viewWillAppear ---20-")
     }
 }
 
